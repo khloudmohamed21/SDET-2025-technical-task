71f822995bbee3c6cadc2ca53e702abb4bac6184
kholoud.mohamed <kholoudmohammed189@gmail.com>
2025-08-12 00:32:33 +0300

    ci commit 15

--- AutomationTask/UI_Task/Task1/nightwatch.conf.js
+++ AutomationTask/UI_Task/Task1/nightwatch.conf.js
@@ -1,53 +1,16 @@
+const chromedriver = require('chromedriver');
 
 module.exports = {
   src_folders: ['test', 'nightwatch/examples'],
-
   page_objects_path: ['nightwatch/page-objects'],
 
-  plugins: [],
-
-  globals_path: '',
-
   test_workers: {
-    enabled: true // ✅ تشغيل الاختبارات على أكثر من عامل
+    enabled: true
   },
 
   test_settings: {
     default: {
-      disable_error_log: false,
       launch_url: 'http://s3-design-sample-site.s3-website-us-west-2.amazonaws.com/',
-
-      screenshots: {
-        enabled: false,
-        path: 'screens',
-        on_failure: true
-      },
-
-      desiredCapabilities: {
-        browserName: 'chrome',
-        acceptInsecureCerts: true,
-        'goog:chromeOptions': {
-          args: [
-            '--no-sandbox',
-            '--disable-dev-shm-usage',
-            '--ignore-certificate-errors',
-            '--headless=new', // أحدث نسخة من الـ headless
-            '--disable-gpu',
-            '--window-size=1920,1080'
-          ]
-        }
-      },
-
-      use_xpath: true,
-
-      webdriver: {
-        start_process: true,
-        server_path: chromedriver.path,
-        port: 9515
-      }
-    },
-
-    chrome: {
       desiredCapabilities: {
         browserName: 'chrome',
         acceptInsecureCerts: true,
@@ -62,7 +25,6 @@ module.exports = {
           ]
         }
       },
-
       webdriver: {
         start_process: true,
         server_path: chromedriver.path,
