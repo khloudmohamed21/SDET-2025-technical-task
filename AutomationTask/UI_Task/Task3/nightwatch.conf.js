e3dfc94d00941ed3b5e7406e709f749a707d3d6d
kholoud.mohamed <kholoudmohammed189@gmail.com>
2025-08-12 12:42:02 +0300

    CI Commit 46

--- AutomationTask/UI_Task/Task3/nightwatch.conf.js
+++ AutomationTask/UI_Task/Task3/nightwatch.conf.js
@@ -30,7 +30,7 @@ module.exports = {
 
       webdriver: {
         start_process: true,
-        server_path: '/usr/lib/chromium-browser/chromedriver',
+        server_path: chromedriver.path,
         port: 9515
       }
 
